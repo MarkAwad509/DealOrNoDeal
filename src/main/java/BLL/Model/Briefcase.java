f333d1c6b41a93d2bfdfd52b18485cd93d55d327
Mark <markawad509@gmail.com>
2022-02-24 05:08:47 -0500

    latest Briefcase class. Still not functional...

--- src/main/java/BLL/Model/Briefcase.java
+++ src/main/java/BLL/Model/Briefcase.java
@@ -1,29 +1,26 @@
 package BLL.Model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Briefcase {
     private static int CASE_NUM_GEN = 1;
     private int caseNumber;
     private double amount;
-    private List<Double> availableValues = new ArrayList(Arrays.asList(0.01, 1.0, 5.0, 10.0, 25.0, 50.0, 75.0, 100.0, 200.0, 300.0,
-    400.0, 500.0, 750.0, 1000.0, 5000.0, 10000.0, 25000.0, 50000.0, 75000.0, 100000.0,
-    200000.0, 300000.0, 400000.0, 500000.0, 750000.0, 1000000.0
-    ));
+    private List<Double> allValues = new ArrayList<>(Arrays.asList(0.01, 1.0, 5.0, 10.0, 25.0, 50.0, 75.0, 100.0, 200.0, 300.0,
+            400.0, 500.0, 750.0, 1000.0, 5000.0, 10000.0, 25000.0, 50000.0, 75000.0, 100000.0,
+            200000.0, 300000.0, 400000.0, 500000.0, 750000.0, 1000000.0));
 
     public Briefcase(){
         this.caseNumber = CASE_NUM_GEN;
-        this.amount = assignValue(availableValues);
-        this.availableValues.remove(amount);
+        int i= generateRandomIndex();
+        this.amount = allValues.get(i);
+        this.allValues.remove(i);
+        //throws UnsupportedOperationException or re-initializes the list everytime.
         CASE_NUM_GEN++;
     }
 
-    private double assignValue(List<Double> values) {
-        //return values[rand]
-        return values.get(new Random().nextInt(0, values.size()));
+    private int generateRandomIndex() {
+        return new Random().nextInt(0, this.allValues.size());
     }
 
     @Override
